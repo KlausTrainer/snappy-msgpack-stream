c29f5ac7f62650fe7908aa907de99f81c430d328
Klaus Trainer <klaus_trainer@posteo.de>
2017-07-11 22:58:11 +0200

    fix: revert previous changes
    BREAKING CHANGE: wire format

--- index.js
+++ index.js
@@ -2,25 +2,30 @@
 
 var pumpify = require('pumpify')
 var through = require('through2')
+var lpStream = require('length-prefixed-stream')
 var msgpack = require('msgpack-lite')
 var snappy = require('snappyjs')
 
 module.exports.createEncodeStream = function SnappyMsgpackEncodeStream (stream) {
-  var msgEncode = msgpack.createEncodeStream()
+  var msgEncode = through.obj(function (data, enc, next) {
+    next(null, msgpack.encode(data))
+  })
 
   var snappyCompress = through.obj(function (data, enc, next) {
     next(null, snappy.compress(data))
   })
 
-  return pumpify.obj(msgEncode, snappyCompress)
+  return pumpify.obj(msgEncode, snappyCompress, lpStream.encode())
 }
 
 module.exports.createDecodeStream = function SnappyMsgpackDecodeStream (stream) {
-  var msgDecode = msgpack.createDecodeStream()
+  var msgDecode = through.obj(function (data, enc, next) {
+    next(null, msgpack.decode(data))
+  })
 
   var snappyUncompress = through.obj(function (data, enc, next) {
     next(null, snappy.uncompress(data))
   })
 
-  return pumpify.obj(snappyUncompress, msgDecode)
+  return pumpify.obj(lpStream.decode(), snappyUncompress, msgDecode)
 }
